a3dab5e90e992793f6e560793eb4e5733164e289
Mrcarrot- Kerbal Space Program <calebharp2005@gmail.com>
2018-02-13 08:34:34 -0600

    Add current source

---
+++ source/Hyperdrive/Hyperdrive/Class1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Hyperdrive
+{
+    class GeneralStuff : MonoBehaviour
+    {
+        void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
+    public class ModuleSomewhereElseDrive : PartModule
+    {
+        //determines an error in time
+        [KSPField]
+        public float timeError = 12000f;
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Take Me Somewhere", unfocusedRange = 90)]
+        public virtual void Hyperdrive()
+        {
+
+            List<CelestialBody> Bodies = FlightGlobals.Bodies;
+            int PlanetCount = Bodies.Count;
+            float RandomLimitValue = (PlanetCount + 1);
+            print("[Hyperdrive]: Bodies loaded. Number of bodies: " + PlanetCount + ".");
+            System.Random rnd = new System.Random();
+            int TargetFGI = rnd.Next(1, PlanetCount);
+            if (Bodies[TargetFGI] != null)
+            {
+                print("[Hyperdrive]: starting. Jumping to body: " + Bodies[TargetFGI] + ".");
+                ScreenMessages.PostScreenMessage("[Hyperdrive]: starting.Jumping to body: " + Bodies[TargetFGI] + ".", 5, ScreenMessageStyle.UPPER_CENTER);
+                WarpDriver.LowTechWarp(Bodies[TargetFGI], timeError);
+            }
+            else
+            {
+                Debug.Log("Exception: List " + Bodies + "Contains no instance of an object with type CelestialBody at list place " + TargetFGI + ". Throwing System.NullReferenceException.");
+                ScreenMessages.PostScreenMessage("Sorry, the hyperdrive cannot jump to that location. We also cannot open the pod bay doors. Blame the engineers.", 5, ScreenMessageStyle.UPPER_CENTER);
+                NullRef();
+                return;
+            }
+        }
+        private static void NullRef()
+        {
+            throw new NullReferenceException();
+        }
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Back Home", unfocusedRange = 90)]
+        public virtual void BackHomeDrive()
+        {
+            List<CelestialBody> Bodies = FlightGlobals.Bodies;
+            ScreenMessages.PostScreenMessage("[Hyperdrive]: starting.Jumping to body: " + Bodies[1] + ".", 5, ScreenMessageStyle.UPPER_CENTER);
+            WarpDriver.LowTechWarp(Bodies[1], 23000);
+        }
+        public override void OnStart(StartState state)
+        {
+            print("PartModule ModuleSomewhereElseDrive loaded.");
+            ScreenMessages.PostScreenMessage("PartModule ModuleSomewhereElseDrive loaded. This partmodule gets a screen message because it's awesome. Deal with it. ", 5, ScreenMessageStyle.UPPER_CENTER);
+        }
+    }
+    public class ModuleSomewhereElsePlus : PartModule
+    {
+        //determines the error in time- notice how much smaller the multiplier is
+        [KSPField]
+        public float timeError = 2f;
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Take Me Somewhere", unfocusedRange = 90)]
+        public virtual void Hyperdrive()
+        {
+
+            List<CelestialBody> Bodies = FlightGlobals.Bodies;
+            int PlanetCount = Bodies.Count;
+            int RandomLimiter = (PlanetCount + 1);
+            print("[Hyperdrive]: Bodies loaded. Number of bodies: " + PlanetCount + ".");
+            System.Random rnd = new System.Random();
+            //Linux is the best- TMSP
+            int TargetFGI = rnd.Next(1, RandomLimiter);
+            if (Bodies[TargetFGI] != null)
+            {
+                ScreenMessages.PostScreenMessage("[Hyperdrive]: starting.Jumping to body: " + Bodies[TargetFGI] + ".", 5, ScreenMessageStyle.UPPER_CENTER);
+                print("[Hyperdrive]: starting. Jumping to body: " + Bodies[TargetFGI] + ".");
+                WarpDriver.MedTechWarp(Bodies[TargetFGI], timeError);
+            }
+            else
+            {
+                ScreenMessages.PostScreenMessage("Sorry, the hyperdrive cannot jump to that location. We also cannot open the pod bay doors. Blame the engineers.", 5, ScreenMessageStyle.UPPER_CENTER);
+                NullRef();
+                return;
+            }
+        }
+
+        private static void NullRef()
+        {
+            throw new NullReferenceException();
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Back to Kerbin", unfocusedRange = 90)]
+        public virtual void BackHomeDrive()
+        {
+            List<CelestialBody> Bodies = FlightGlobals.Bodies;
+            WarpDriver.MedTechWarp(Bodies[1], 23000);
+        }
+        public override void OnStart(StartState state)
+        {
+            print("PartModule ModuleSomewhereElseDrive loaded.");
+        }
+    }
+    public class ModuleSelectiveHyperdrive : PartModule
+    {
+        List<CelestialBody> Bodies = FlightGlobals.Bodies;
+        /*Template for a Hyperdrive KSPEvent:
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "NAME_HERE", unfocusedRange = 90)]
+        public virtual void NAME_HERE()
+        {
+            WarpDriver.MedTechWarp(Bodies[FGI_HERE], 23000);
+        }
+        */
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Back to Kerbin", unfocusedRange = 90)]
+        public virtual void BackHomeDrive()
+        {
+            WarpDriver.MedTechWarp(Bodies[1], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Mun", unfocusedRange = 90)]
+        public virtual void Mun()
+        {
+            WarpDriver.MedTechWarp(Bodies[2], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Minmus", unfocusedRange = 90)]
+        public virtual void Minmus()
+        {
+            WarpDriver.MedTechWarp(Bodies[3], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Moho", unfocusedRange = 90)]
+        public virtual void Moho()
+        {
+            WarpDriver.MedTechWarp(Bodies[4], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Eve", unfocusedRange = 90)]
+        public virtual void Eve()
+        {
+            WarpDriver.MedTechWarp(Bodies[5], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Duna", unfocusedRange = 90)]
+        public virtual void Duna()
+        {
+            WarpDriver.MedTechWarp(Bodies[6], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Ike", unfocusedRange = 90)]
+        public virtual void ILikeIke()
+        {
+            WarpDriver.MedTechWarp(Bodies[7], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Jool", unfocusedRange = 90)]
+        public virtual void JoolIsCool()
+        {
+            WarpDriver.MedTechWarp(Bodies[8], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Laythe", unfocusedRange = 90)]
+        public virtual void Laythe()
+        {
+            WarpDriver.MedTechWarp(Bodies[9], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Vall", unfocusedRange = 90)]
+        public virtual void Vall()
+        {
+            WarpDriver.MedTechWarp(Bodies[10], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Bop", unfocusedRange = 90)]
+        public virtual void BoppingBop()
+        {
+            WarpDriver.MedTechWarp(Bodies[11], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Tylo", unfocusedRange = 90)]
+        public virtual void Tylo()
+        {
+            WarpDriver.MedTechWarp(Bodies[12], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Gilly", unfocusedRange = 90)]
+        public virtual void ThatOneSpeck()
+        {
+            WarpDriver.MedTechWarp(Bodies[13], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Pol", unfocusedRange = 90)]
+        public virtual void Pol()
+        {
+            WarpDriver.MedTechWarp(Bodies[14], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Dres", unfocusedRange = 90)]
+        public virtual void NobodyLikesDres()
+        {
+            WarpDriver.MedTechWarp(Bodies[15], 23000);
+        }
+
+        [KSPEvent(externalToEVAOnly = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = true, guiName = "Jump to Eeloo", unfocusedRange = 90)]
+        public virtual void Eeloo()
+        {
+            WarpDriver.MedTechWarp(Bodies[16], 23000);
+        }
+    }
+}
---
+++ source/Hyperdrive/Hyperdrive/JoolWarp.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Hyperdrive
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class WarpDriver : MonoBehaviour
+    {
+        public static void LowTechWarp(CelestialBody TargetBody, float TimeFactor)
+        {
+            print("Void MrcarrotHyperdrive.WarpDriver.LowTechWarp is Triggered. Beginning jump drive action.");
+            var orbit = Orbit.CreateRandomOrbitFlyBy(TargetBody, 0.5);
+            ///These warp drives edit reality in a way that has some issues-
+            ///one of them being going forward in time 20000 seconds- and even
+            ///more for Kerbin warping
+            var OldUT = Planetarium.GetUniversalTime();
+            var UTChange = TimeFactor * 1.667;
+            var NewUT = (OldUT + UTChange);
+            Planetarium.SetUniversalTime(NewUT);
+            print("[Hyperdrive]: UT updated. New UT: " + NewUT + ". Jumping.");
+
+            if (FlightGlobals.ActiveVessel != null)
+            {
+                print("Activating WarpDrive, property FlightGlobals.ActiveVessel has a value.");
+                OrbitPhysicsManager.HoldVesselUnpack(60);
+                FlightGlobals.ActiveVessel.GoOnRails();
+                FlightGlobals.ActiveVessel.orbit.referenceBody = orbit.referenceBody;
+                FlightGlobals.ActiveVessel.orbit.LAN = orbit.LAN;
+                FlightGlobals.ActiveVessel.orbit.semiMajorAxis = orbit.semiMajorAxis;
+                FlightGlobals.ActiveVessel.orbit.eccentricity = orbit.eccentricity;
+                FlightGlobals.ActiveVessel.orbit.inclination = orbit.inclination;
+                FlightGlobals.ActiveVessel.orbit.argumentOfPeriapsis = orbit.argumentOfPeriapsis;
+                FlightGlobals.ActiveVessel.orbit.meanAnomalyAtEpoch = orbit.meanAnomalyAtEpoch;
+                FlightGlobals.ActiveVessel.orbit.epoch = orbit.epoch;
+                FlightGlobals.ActiveVessel.orbitDriver.orbit = orbit;
+                FlightGlobals.ActiveVessel.orbitDriver.orbit.Init();
+                FlightGlobals.ActiveVessel.orbitDriver.orbit.UpdateFromUT(Planetarium.GetUniversalTime());
+            }
+            else
+            {
+                print("Error. Could not initiate Jump Drive. Property FlightGlobals.ActiveVessel is null.");
+                print("How on Kerbin did you even call this void if there is no active vessel?");
+                return;
+            }
+        }
+        public static void MedTechWarp(CelestialBody TargetBody, float UTChange)
+        {
+            print("Void MrcarrotHyperdrive.WarpDriver.MedTechWarp is Triggered. Beginning jump drive action.");
+
+            var orbit = Orbit.CreateRandomOrbitAround(TargetBody, TargetBody.Radius + 200000, TargetBody.Radius + 200000);
+            var OldUT = Planetarium.GetUniversalTime();
+            var NewUT = (OldUT + UTChange);
+            Planetarium.SetUniversalTime(NewUT);
+            print("[Hyperdrive]: UT updated. Jumping.");
+
+            if (FlightGlobals.ActiveVessel != null)
+            {
+                print("Activating WarpDrive, property FlightGlobals.ActiveVessel has a value.");
+                OrbitPhysicsManager.HoldVesselUnpack(60);
+                FlightGlobals.ActiveVessel.GoOnRails();
+                FlightGlobals.ActiveVessel.orbit.referenceBody = orbit.referenceBody;
+                FlightGlobals.ActiveVessel.orbit.LAN = orbit.LAN;
+                FlightGlobals.ActiveVessel.orbit.semiMajorAxis = orbit.semiMajorAxis;
+                FlightGlobals.ActiveVessel.orbit.eccentricity = orbit.eccentricity;
+                FlightGlobals.ActiveVessel.orbit.inclination = orbit.inclination;
+                FlightGlobals.ActiveVessel.orbit.argumentOfPeriapsis = orbit.argumentOfPeriapsis;
+                FlightGlobals.ActiveVessel.orbit.meanAnomalyAtEpoch = orbit.meanAnomalyAtEpoch;
+                FlightGlobals.ActiveVessel.orbit.epoch = orbit.epoch;
+                FlightGlobals.ActiveVessel.orbitDriver.orbit = orbit;
+                FlightGlobals.ActiveVessel.orbitDriver.orbit.Init();
+                FlightGlobals.ActiveVessel.orbitDriver.orbit.UpdateFromUT(Planetarium.GetUniversalTime());
+            }
+            else
+            {
+                print("Error. Could not initiate Jump Drive. Property FlightGlobals.ActiveVessel is null.");
+                print("How on Kerbin did you even call this void if there is no active vessel?");
+                return;
+            }
+        }
+    }
+}
