a244f94502b8306907e5930e02752cb508e1f38f
Mrcarrot- Kerbal Space Program <calebharp2005@gmail.com>
2018-02-26 19:13:27 -0600

    I have no clue what most of this is tbh

--- source/Hyperdrive/ConfigParser/NodeParser.cs
+++ source/Hyperdrive/ConfigParser/NodeParser.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Collections.Generic;
 
-namespace HyperDrive
+namespace Hyperdrive
 {
-  public class NodeParser
-  {
-    public bool globalImpDrive;
-  }
+	public class NodeParser
+	{
+		public bool globalImpDrive;
+	}
 }
