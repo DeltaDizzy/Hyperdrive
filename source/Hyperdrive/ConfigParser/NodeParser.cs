b869afef1fcba7df9e0d77709ce9fe3d600f557b
DeltaDizzy <EJBraswell@gmail.com>
2018-04-04 20:35:43 -0500

    Update NodeParser.cs

--- source/Hyperdrive/ConfigParser/NodeParser.cs
+++ source/Hyperdrive/ConfigParser/NodeParser.cs
@@ -5,6 +5,21 @@ namespace Hyperdrive
 {
 	public class NodeParser
 	{
-		public bool globalImpDrive;
+		//Declare ConfigNode field and arroy of all configs. This will be important later
+		public ConfigNode HDNode;
+		UrlDir.UrlConfig[] nodeList;
+		
+		//Find all HYPERDRIVE_GLOBAL nodes, and store them in the nodeList
+		nodeList = GameDatabase.Instance.GetConfigs("HYPERDRIVE_GLOBAL");
+		
+		if(nodeList.Length > 1)
+		{
+			Debug.LogWarning("No HYPERDRIVE_GLOBAL nodes found. What were/are you thinking?!")
+		}
+		
+		foreach(UrlDir.UrlConfig config in nodeList)
+            	{
+			
+            	}
 	}
 }
